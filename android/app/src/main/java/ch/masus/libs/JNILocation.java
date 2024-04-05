c7ee5cd842b4d44a302e453ed2dbb73c36c966db
masus <thomas.steinmann@protonmail.com>
2024-04-05 11:33:17 +0200

    Added location dependency

--- android/app/src/main/java/ch/masus/libs/JNILocation.java
+++ android/app/src/main/java/ch/masus/libs/JNILocation.java
@@ -1,6 +1,13 @@
 package ch.masus.libs;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import androidx.annotation.Keep;
+
+
+@Keep
 public class JNILocation {
+
+    public FusedLocationProviderClient client;
 
     public void sayHi() {
         System.out.println("Hi Team!");
